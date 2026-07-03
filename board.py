f91cfa8130d2ee9cc819536ffb1be61a76d446c6
lstsa <luis.lustosa1981@gmail.com>
2026-07-03 16:45:52 -0300

    feat: adiciona validação de linha, coluna e quadrante

--- board.py
+++ board.py
@@ -2,7 +2,6 @@ def criar_board():
     board =[]
     line = []
     
-    
     for i in range(9):
         for j in range(9):
             line.append(0)
@@ -20,6 +19,27 @@ def exibir_board(board):
                 print("|", end=" ")
             print(column, end=" ")
         print()
-                        
+        
+def valida_linha(board, linha, numero):
+    return numero not in board[linha]
+
+def valida_coluna(board, coluna, numero):
+    col = [board[i][coluna] for i in range(9)]
+    return numero not in col
+
+def valida_quadrante(board, linha, coluna, numero):
+    start_row = (linha // 3) * 3
+    start_col = (coluna // 3) * 3
+    for i in range(3):
+        for j in range(3):
+            if board[start_row + i][start_col + j] == numero:
+                return False
+    return True
+
+def jogada_valida(board,linha, coluna, numero):
+    return (valida_linha(board, linha, numero) and
+            valida_coluna(board, coluna, numero) and
+            valida_quadrante(board, linha, coluna, numero))
+
 board = criar_board()
 exibir_board(board)
